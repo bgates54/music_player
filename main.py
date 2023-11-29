ba723ae06f2ab7d5a4fafec5ec5e210f34849f3a
Brynna Gates <bgates@my.waketech.edu>
2023-11-28 22:41:50 -0500

    Added song

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from player import Player
 
 
 def main():
-    songs = ["Jingle Bells", 
-             "Santa Claus is Coming to Town", 
-             "Winter Wonderland",
-             "Christmas Carol",
-             "All I Want for Christmas is You",
-             "I'll Be Home for Christmas",
-             "It's Beginning to Look a Lot Like Christmas",
-             "Last Christmas",
-             "Let It Snow",
-             "Rudolph the Red-Nosed Reindeer",]
+    songs = [
+        "Jingle Bells",
+         "Santa Claus is Coming to Town",
+         "Winter Wonderland",
+         "Christmas Carol",
+         "All I Want for Christmas is You",
+         "I'll Be Home for Christmas",
+         "It's Beginning to Look a Lot Like Christmas",
+         "Last Christmas",
+         "Let It Snow",
+         "White Christmas",
+         "Rudolph the Red-Nosed Reindeer",
+    ]
     music_player = Player(songs)
     music_player.run()
 
